c3f9ce9956e24ef36046f48f22a8d7629d654379
Habib Fauzan Akbar <habibakbarfauzan@gmail.com>
2021-03-23 13:24:49 +0700

    Mengatasi perubahan konfigurasi

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -28,6 +28,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_VALUE = "dessert_sold_key"
+
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
@@ -76,12 +79,25 @@ class MainActivity : AppCompatActivity() {
         }
         dessertTimer = DessertTimer(this.lifecycle)
 
+        if( savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_VALUE,0)
+            showCurrentDessert()
+        }
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
 
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_VALUE, dessertsSold)
+
     }
 
     override fun onStart() {
@@ -180,4 +196,5 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
